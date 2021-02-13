c1cff8761640b2e1fc288697e17a28b7fda434d5
Valentin <valentin.rudloff.perso@gmail.com>
2021-02-13 23:07:04 +0100

    read credential / connect to Open sky api / get flights in the specific area

--- track_flights.py
+++ track_flights.py
@@ -1,1 +1,13 @@
 from opensky_api import OpenSkyApi, StateVector
+
+# Get credentials
+f = open("credential.txt", "r")
+credential = f.read().splitlines()
+
+# Cergy GPS Bounding Box
+cergy_location = (49.013780, 49.057670, 1.991615, 2.086029)
+
+# Get flights in the specific location
+api = OpenSkyApi(credential[0], credential[1])
+states = api.get_states(bbox=cergy_location)
+print(states)
