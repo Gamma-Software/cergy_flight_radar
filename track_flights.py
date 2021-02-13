ffe66088f97aa74ef16c4d4ec3b3dad89f8bc25f
Valentin <valentin.rudloff.perso@gmail.com>
2021-02-13 23:27:34 +0100

    add functions to get clearer

--- track_flights.py
+++ track_flights.py
@@ -4,49 +4,48 @@ import csv
 import os.path
 from os import path
 
-# Get credentials
-f = open("credential.txt", "r")
-credential = f.read().splitlines()
+
+def credentials(filename):
+    # Get credentials
+    f = open(filename, "r")
+    return f.read().splitlines()
+
+
+def get_flights_from_loc(usr, pwd, loc):
+    # Get flights in the specific location
+    api = OpenSkyApi(usr, pwd)
+    return api.get_states(bbox=loc)
+
+
+def save_flights(with_header=False):
+    if with_header:
+        with open("flights_over_cergy.csv", 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=columns)
+            writer.writeheader()
+    with open("flights_over_cergy.csv", 'a') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=columns)
+        # iterate over flights in the current time flying above Cergy
+        dict_to_save = {'timestamp': states.time}
+        if states.states:
+            for flight in states.states:
+                dict_to_save.update(flight.__dict__)
+                writer.writerow(dict_to_save)
+        else:
+            writer.writerow(dict_to_save)
+
+
+credential = credentials("credential.txt")
 
 # Cergy GPS Bounding Box
 cergy_location = (49.013780, 49.057670, 1.991615, 2.086029)
-
-# Get flights in the specific location
-api = OpenSkyApi(credential[0], credential[1])
-states = api.get_states(bbox=cergy_location)
-#states = api.get_states()
+states = get_flights_from_loc(credential[0], credential[1], cergy_location)
 
 columns = StateVector.keys
 columns.insert(0, 'timestamp')
 
 # Save those flights
 try:
-    if not path.exists("flights_over_cergy.csv"):
-        with open("flights_over_cergy.csv", 'w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=columns)
-            writer.writeheader()
-        with open("flights_over_cergy.csv", 'a') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=columns)
-            # iterate over flights in the current time flying above Cergy
-            dict_to_save = {'timestamp': states.time}
-            if states.states:
-                for flight in states.states:
-                    dict_to_save.update(flight.__dict__)
-                    writer.writerow(dict_to_save)
-            else:
-                writer.writerow(dict_to_save)
-    else:
-        with open("flights_over_cergy.csv", 'a') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=columns)
-            # iterate over flights in the current time flying above Cergy
-            dict_to_save = {'timestamp': states.time}
-            if states.states:
-                for flight in states.states:
-                    dict_to_save.update(flight.__dict__)
-                    writer.writerow(dict_to_save)
-            else:
-                writer.writerow(dict_to_save)
-
+    save_flights(not path.exists("flights_over_cergy.csv"))
 except IOError:
     print("I/O error")
 
